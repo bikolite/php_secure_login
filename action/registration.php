b93067464e3a91dc0ad30b4e5b5dacd2ebfe2e9b
bikolite <bikolite.info@gmail.com>
2024-02-22 00:40:07 +0600

    Date 21.02.24

--- action/registration.php
+++ action/registration.php
@@ -1,28 +1,28 @@
 <?php
 if (isset($_POST["submit"])) {
-    $fullName = $_POST["name"];
+    $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
     $errors = array();
-    if (empty($fullName) or empty($email) or empty($password)) {
-        array_push($errors, "All Fields are Required");
+    if (empty($name) or empty($email) or empty($password)) {
+        array_push($errors, "Fields are Required");
     }
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        array_push($errors, "Email is not valid");
+        array_push($errors, "Your Email is not valid");
     }
     if (strlen($password) < 8) {
-        array_push($errors, "Password must be at least 8 charactes long");
+        array_push($errors, "Password must be 8 charactes.");
     }
 
-    // require_once "config/database.php";
+    // set database into local server;
     require_once "../config/database.php";
     $sql = "SELECT * FROM users WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
     $rowCount = mysqli_num_rows($result);
     if ($rowCount > 0) {
-        array_push($errors, "Email already exists!");
+        array_push($errors, "Your Email already exists! Please Login");
         header("Location: ../index.php");
     }
     if (count($errors) > 0) {
@@ -35,7 +35,7 @@ if (isset($_POST["submit"])) {
         $stmt = mysqli_stmt_init($conn);
         $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
         if ($prepareStmt) {
-            mysqli_stmt_bind_param($stmt, "sss", $fullName, $email, $passwordHash);
+            mysqli_stmt_bind_param($stmt, "sss", $name, $email, $passwordHash);
             mysqli_stmt_execute($stmt);
             header("Location: ../index.php");
             exit();
